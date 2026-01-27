d4269f65d7a421b8510631db7d2bde13da95e070
firmansyah12andre <firmansyah12andre@gmail.com>
2026-01-27 23:17:33 +0700

    Add Web UI for Shodan search

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask, request
+from flask import render_template
 import shodan
 
 app = Flask(__name__)
 
 SHODAN_API_KEY = os.environ.get("SHODAN_API_KEY")
 api = shodan.Shodan(SHODAN_API_KEY)
+
+@app.route("/ui")
+def ui():
+    return render_template("index.html")
 
 @app.route("/")
 def index():
